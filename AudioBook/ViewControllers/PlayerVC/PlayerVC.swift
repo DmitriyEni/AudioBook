bca38c59d6b2108445c0c1488a7ad4da76d98d0b
DmitriyEni <eni.dmitriy@mail.ru>
2022-06-21 01:12:35 +0300

    UIfromCode

--- AudioBook/ViewControllers/PlayerVC/PlayerVC.swift
+++ AudioBook/ViewControllers/PlayerVC/PlayerVC.swift
@@ -12,6 +12,11 @@ import Haptica
 
 class PlayerVC: UIViewController {
     
+    public var position = 0
+    public var book: [Book] = []
+    
+    @IBOutlet weak var holder: UIView!
+    
     @IBOutlet weak var playPauseButton: UIButton!
     
     @IBOutlet weak var numberOfChaptersLabel: UILabel!
@@ -20,22 +25,62 @@ class PlayerVC: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var coverImage: UIImageView!
-    
-    public var book = Book(name: "", albomName: "", artistName: "", imageName: "", tackName: "")
+
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        numberOfChaptersLabel.text = book.name
-        nameLabel.text = book.albomName
-        coverImage.image = UIImage(named: book.imageName)
+//        numberOfChaptersLabel.text = book.name
+//        nameLabel.text = book.albomName
+//        coverImage.image = UIImage(named: book.imageName)
     }
     
-//    func createPlayer(Cover: String, Name: String, Chapters: String) {
-//        nameLabel.text = Name
-//        coverImage.image = UIImage(named: Cover)
-//        numberOfChaptersLabel.text = Chapters
-//
-//    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if holder.subviews.count == 0 {
+            configure()
+        }
+    }
+    
+    func configure() {
+        // set up player
+        let song = book[position]
+        print("\(song)")
+        
+        let urlString = Bundle.main.path(forResource: song.tackName, ofType: "mp3")
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let urlString = urlString
+            else {
+
+                return
+            }
+            print("\(urlString)")
+
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                return
+            }
+            
+            player.play()
+
+        }
+        catch {
+            print("Error")
+        }
+        // set up user interface elements
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let player = player {
+            player.stop()
+        }
+    }
     
     @IBAction func playPauseButtonAction(_ sender: Any) {
         Haptic.impact(.soft).generate()
